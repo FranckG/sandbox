6c4f3b6420b771a66066525c2b57f5a2c1de8f2f
Franck GILLIERS <franck.gilliers@thalesgroup.com>
2015-01-22 17:13:26 +0100

    update gitcase

--- gitcase.bat
+++ gitcase.bat
@@ -1,3 +1,3 @@
 set VIEW_TAG=fgi_ProjectA_int
 cleartool startview %VIEW_TAG%
-clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%
+clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%\Test_comp\Test_CCEnv
