c3d8171b704ba64b4f5a4c7ddb64c1ad18ab4cb6
Franck GILLIERS <franck.gilliers@thalesgroup.com>
2015-01-22 18:54:24 +0100

    update gitcase

--- gitcase.pl
+++ gitcase.pl
@@ -1,12 +1,12 @@
 my $url="http://vmo10113:8080/rest/api/2/issue/STOF-5?fields=customfield_10010";
 my $curl=`curl -su  jenkins:tcinteg -H "Accept: application/json" -X GET $url`;
 
-`git clean -dfx`
-`git reset --hard HEAD`
+`git clean -dfx`;
+`git reset --hard HEAD`;
 
 #print "$curl\n";
-my $id = $curl =~ /"customfield_10010":"(.*)"}}/
+my $id = $curl =~ /"customfield_10010":"(.*)"}}/;
 
 my $viewTag=fgi_ProjectA_int;
-`cleartool startview $viewTag`
+`cleartool startview $viewTag`;
 print "clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%\Test_comp\Test_CCEnv"
