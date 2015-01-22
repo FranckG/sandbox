fbf858f8f46efeb9f898db354464dc026bd9579b
Franck GILLIERS <franck.gilliers@thalesgroup.com>
2015-01-22 17:44:11 +0100

    update gitcase

--- gitcase.bat
+++ gitcase.bat
@@ -1,3 +1,12 @@
+REM 
+REM CLEAN GIT REPO
+REM
+git clean -dfx
+git reset -hard HEAD
+
+REM
+REM CLEARFSIMPORT
+REM
 set VIEW_TAG=fgi_ProjectA_int
 cleartool startview %VIEW_TAG%
-clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%\Test_comp\Test_CCEnv
+REM clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%\Test_comp\Test_CCEnv
