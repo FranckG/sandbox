6642a0f0d5f96bd34850baec6a790841172c1105
Franck GILLIERS <franck.gilliers@thalesgroup.com>
2015-01-22 18:54:58 +0100

    update gitcase

--- gitcase.pl
+++ gitcase.pl
@@ -6,7 +6,7 @@ my $curl=`curl -su  jenkins:tcinteg -H "Accept: application/json" -X GET $url`;
 
 #print "$curl\n";
 my $id = $curl =~ /"customfield_10010":"(.*)"}}/;
-
+print "id = $id";
 my $viewTag=fgi_ProjectA_int;
 `cleartool startview $viewTag`;
 print "clearfsimport -recurse -rmname -nsetevent . m:\%VIEW_TAG%\Test_comp\Test_CCEnv"
