e840c5bd4aaf9ef27fe39334f675d495406a291c
Franck GILLIERS <franck.gilliers@thalesgroup.com>
2015-01-26 16:10:09 +0100

    update fitcase.py

--- gitcase.py
+++ gitcase.py
@@ -16,15 +16,20 @@ componentRootDir = 'Test_comp/Test_CCEnv';
 # GET ENVIRONMENT
 gitCommit = "8f5f090ea5d977d5d3097818b3980715d9553a70";
 #gitCommit = os.environ['GIT_COMMIT']
-print "git commit: '"+gitCommit+"'"
+print ("git commit: '"+gitCommit+"'")
 
 repoUrl = "http://150.2.38.125:7990/scm/ssp/wakeupclock.git";
 #repoUrl = os.environ['GIT_URL']
-print "repository URL: '"+repoUrl+"'";
+print ("repository URL: '"+repoUrl+"'")
 
-branch = "origin/develop";
 #branch = os.environ['GIT_BRANCH'];
-print "Git branch: '"+branch+"'";
+branch = integrationBranch
+print ("Git branch: '"+branch+"'")
+
+import sys
+if branch != integrationBranch:   # can be retrieve dynamically ?
+  print ("Not working on develop branch: nothing to sync with ClearQuest")
+  sys.exit(0)
 
 
 matchObj=re.match('https{0,}://.+:\d+/scm/(.+)/(.+)\.git', repoUrl)
@@ -32,10 +37,10 @@ if matchObj:
   projectKey = matchObj.group(1)
   repositorySlug =  matchObj.group(2)
 else:
-  print "No match"
+  print ('No match')
 
-print "project key:     '"+projectKey+"'"
-print "repository slug: '"+repositorySlug+"'"
+print ("project key:     '"+projectKey+"'")
+print ("repository slug: '"+repositorySlug+"'")
 
 
 #########################################################################
@@ -45,7 +50,7 @@ r = requests.get(url, auth=(user, password))
 jsonResponse = r.json()
 #print json.dumps(jsonResponse, indent=4, sort_keys=True)
 jiraId = jsonResponse['attributes']['jira-key'][0]
-print "Jira ID: '"+jiraId+"'";
+print ("Jira ID: '"+jiraId+"'")
 
 #########################################################################
 # GET ClearQuest ID
@@ -54,7 +59,7 @@ r = requests.get(url, auth=(user, password))
 jsonResponse = r.json()
 #print json.dumps(jsonResponse, indent=4, sort_keys=True)
 clearQuestId = jsonResponse['fields']['customfield_10010']
-print "ClearQuest ID: '"+clearQuestId+"'";
+print ("ClearQuest ID: '"+clearQuestId+"'")
 
 return_code = subprocess.call(['cleartool', 'startview', viewTag], shell=True)
 return_code = subprocess.call(['cleartool', 'setactivity', '-c', "link to Jira '"+jiraId+"'", '-view', viewTag, clearQuestId], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -62,7 +67,7 @@ return_code = subprocess.call(['cleartool', 'setactivity', '-c', "link to Jira '
 # PREPARE SOURCE FOLDER
 return_code = subprocess.call(['git', 'clean', '-dfx'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-import sys
+
 sys.exit(0)
 
 return_code = subprocess.call(['git', 'reset', '--hard', 'HEAD'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
